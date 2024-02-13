d16caace7b07aade741f6960ae24c285d84d92c2
Portatarga <paolo.deberardinis@libero.it>
2024-02-13 16:49:20 +0100

    Ultima Modifica

--- Zoo2/src/Main.java
+++ Zoo2/src/Main.java
@@ -75,20 +75,18 @@ public class Main {
 
     public static void main(String[] args) {
         // Populate the ArrayLists
-        ArrayList<Lion> lions = new ArrayList<>();
-        lions.add(new Lion("aaaa", "meat", 10, LocalDate.of(2023, 12, 20), 150, 150, 70));
-        lions.add(new Lion("bbbb", "meat", 13, LocalDate.of(2023, 11, 10), 135, 145, 60));
-        lions.add(new Lion("cccc", "meat", 6, LocalDate.of(2023, 5, 15), 160, 130, 100));
-
-        ArrayList<Tiger> tigers = new ArrayList<>();
-        tigers.add(new Tiger("ddddd", "meat", 5, LocalDate.of(2023, 2, 24), 135, 130, 100));
-        tigers.add(new Tiger("eeeee", "meat", 7, LocalDate.of(2023, 7, 14), 140, 135, 65));
-        tigers.add(new Tiger("fffff", "meat", 11, LocalDate.of(2022, 9, 15), 155, 150, 45));
-
-        ArrayList<Eagle> eagles = new ArrayList<>();
-        eagles.add(new Eagle("gggg", "meat", 3, LocalDate.of(2023, 1, 1), 7, 35, 220));
-        eagles.add(new Eagle("hhhhh", "fish", 2, LocalDate.of(2023, 5, 8), 5, 30, 180));
-        eagles.add(new Eagle("iiiii", "meat", 5, LocalDate.of(2022, 10, 18), 8, 45, 200));
+        ArrayList<Lion> lions = new ArrayList();
+        lions.add(new Lion("Simba", "meat", 10, LocalDate.of(2023, 12, 20), 150.0, 150.0, 90.0));
+        lions.add(new Lion("Loki", "meat", 13, LocalDate.of(2023, 11, 10), 135.0, 145.0, 60.0));
+        lions.add(new Lion("Jessy", "meat", 6, LocalDate.of(2023, 5, 15), 160.0, 130.0, 50.0));
+        ArrayList<Tiger> tigers = new ArrayList();
+        tigers.add(new Tiger("Smokey", "meat", 5, LocalDate.of(2023, 2, 24), 135.0, 130.0, 55.0));
+        tigers.add(new Tiger("Vice", "meat", 7, LocalDate.of(2023, 7, 14), 140.0, 135.0, 90.0));
+        tigers.add(new Tiger("Nerone", "meat", 11, LocalDate.of(2022, 9, 15), 155.0, 150.0, 45.0));
+        ArrayList<Eagle> eagles = new ArrayList();
+        eagles.add(new Eagle("Achille", "meat", 3, LocalDate.of(2023, 1, 1), 7.0, 35.0, 220.0));
+        eagles.add(new Eagle("Thiago", "fish", 2, LocalDate.of(2023, 5, 8), 5.0, 30.0, 180.0));
+        eagles.add(new Eagle("Draco", "meat", 5, LocalDate.of(2022, 10, 18), 8.0, 45.0, 200.0));
 
         // Calculation and printing of the heaviest lion
         Lion heaviestLion = findMax(lions, "weight");
