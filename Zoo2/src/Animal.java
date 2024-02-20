a9ff53a5f5d82a327303d472030eec86c70b2a57
Portatarga <paolo.deberardinis@libero.it>
2024-02-20 16:02:32 +0100

    Modifiche già effettuate

--- Zoo2/src/Animal.java
+++ Zoo2/src/Animal.java
@@ -1,12 +1,12 @@
 import java.time.LocalDate;
 
 class Animal {
-    private final String name;
-    private final String favoriteFood;
-    private final int age;
-    private final LocalDate entryDate;
-    private final double weight;
-    private final double height;
+    private String name;
+    private String favoriteFood;
+    private int age;
+    private LocalDate entryDate;
+    private double weight;
+    private double height;
 
     Animal(String name, String favoriteFood, int age, LocalDate entryDate, double weight, double height) {
         this.name = name;
@@ -21,11 +21,47 @@ class Animal {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public void setFavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
     public double getWeight() {
         return weight;
     }
 
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
     public double getHeight() {
         return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
     }
 }
